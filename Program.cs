7c39039c9a9e1ef3a8cb184e8839b1f3a10f7d67
John Singer <jpsthecelt@gmail.com>
2021-03-31 19:42:35 -0700

    first pass

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace serial_read
+{
+    class Program
+   {
+     static SerialPort _serialPort;
+     static void Main(string[] args)
+     {
+      bool notStop = true;
+
+       Console.Write("Port no: ");
+       string port = Console.ReadLine();
+    //    Console.Write("baudrate: ");
+       string baudrate = "9600";
+   
+       // Create a new SerialPort on indicated serial device (port)
+       _serialPort = new SerialPort(port, int.Parse(baudrate));
+   
+       // Set the read/write timeouts
+       _serialPort.ReadTimeout = 1500;
+       _serialPort.WriteTimeout = 1500;
+       _serialPort.Open();
+
+       do {
+         Read();
+       } while (notStop);
+
+       _serialPort.Close();
+     }
+   
+     public static void Read()
+     {
+       try
+       {
+         string message = _serialPort.ReadLine();
+         Console.WriteLine(message);
+       }
+       catch (TimeoutException) { }
+     }
+   }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+This is serial-read (Program.cs), wherein I perform a C#/.NET read of the indicated
+port on the Raspberry Pi.
+
+This is a prelude to a full-blown GPS system which will read GPS messages from a 
+NEO-6 u-blox type device, streaming NMEA messages.
+
+It is based on the .NET 5.0 serial example from the uSoft documentation.
+
+The u-BLOX is connected to an AtMega-type serial-to-USB plug-in converter
+
+The original inspiration was created/saved elsewhere in github.com/jpsthecelt, originally
+as a python program.
+
+3.30.21-jps
