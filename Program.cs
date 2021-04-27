94c1587024a78d5419f2ebdb9d346da7972dc4cb
jpsthecelt <jpsthecelt@gmail.com>
2021-04-26 20:28:08 -0700

    slimming down logic

--- Program.cs
+++ Program.cs
@@ -14,12 +14,7 @@ namespace serial_read
      {
       bool notDone = true;
 	    var msgQueue = Channel.CreateUnbounded<string>();
-      
-
-      //  Console.Write("Port no: ");
-      //  string port = Console.ReadLine();
-    //    Console.Write("baudrate: ");
-       string baudrate = "9600";
+      string baudrate = "9600";
    
        // Create a new SerialPort on indicated serial device (port)
        _serialPort = new SerialPort(port, int.Parse(baudrate));
@@ -31,42 +26,20 @@ namespace serial_read
 
       _ = Task.Factory.StartNew(async () => 
         {
-          // for (int i=0; i<50; i++) {
           while (notDone) {
                 string bufr = _serialPort.ReadLine();
-                Console.WriteLine($"read & sent: {bufr}");
-              await msgQueue.Writer.WriteAsync(bufr);
+                Console.WriteLine($"forwarded: {bufr.Split(",")[0]}");
+                await msgQueue.Writer.WriteAsync(bufr);
           } 
           msgQueue.Writer.Complete();
         });
 
-await foreach (var item in msgQueue.Reader.ReadAllAsync())
-{
-  Console.WriteLine($"Received: {item}");
-  // await Task.Delay(250);
-}
+      await foreach (var item in msgQueue.Reader.ReadAllAsync())
+      {
+        Console.WriteLine($"Adding to DB: {item}");
+        await Task.Delay(100);
+      }
        _serialPort.Close();
      }
-   
-    // public static Read() {
-        // Console.WriteLine($"Thread={Thread.CurrentThread.ManagedThreadId} Write a sentence and see each word reversed: ");
-        // var msg = Console.ReadLine();
-        // Console.WriteLine("");
-    // 
-        // foreach (var s in msg.Split())
-        // {
-        	  // await messageReverser.QueueForProcessing(s, cancellationTokenSource.Token);
-        // }
-    //  }
-
-    //  public static void oldRead()
-    //  {
-      //  try
-      //  {
-        //  string message = _serialPort.ReadLine();
-        //  Console.WriteLine(message);
-      //  }
-      //  catch (TimeoutException) { }
-    //  }
    }
 }
